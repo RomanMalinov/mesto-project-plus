056b12e9507475413ae961dbbe9378d712c3d8d5
Roman <malinovroman@mail.ru>
2024-05-15 15:12:43 +0300

    создание иИнфраструктуры  проекта

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,15 @@
+import express from "express";
+import mongoose from "mongoose";
 
+const app = express();
 
-const app = () => {
-  1+1
-};
+mongoose.connect("mongodb://localhost:27017/mydb");
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.status(200).json("test")
+});
+
+app.listen(PORT, () => {
+  console.log(`Сервер запущен на порту ${PORT}`);
+});
